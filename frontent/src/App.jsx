ecefd6ec0b59c0112809af0487873fa648ef50da
Nurul Hasan <2020kucp1109@iiitkota.ac.com>
2023-12-26 17:33:24 +0530

    first commit

--- frontent/src/App.jsx
+++ frontent/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './comp/Header/Header'
 import List from "./comp/List/List"
@@ -17,37 +16,41 @@ function App() {
 
   return (
     <>
-<CssVarsProvider defaultMode="system">
-    <Header></Header>
-    <div className='row'>
-      <div className="col-12 p-0">
-        <Slider></Slider>
-      </div>
-    </div>
-    <div className="overlay"></div>
-    
-    <Grid className="bg-dark py-4" container spacing={3} sx={{ flexGrow: 1 }}>
-    <Grid className="my-3" lg={12}>
-<Typography
- color="neutral"
-  level="h1"
-  textAlign={'center'}>
-   Most Valuable Properties of Kota under one Roof
-    </Typography>
-   <Typography  className="mb-3" textAlign={'center'}>
-    At Afordable Price at <Chip> 0% brokerage </Chip>
-  </Typography>
-    
-  </Grid>
-      <Grid  xs={12} md={3}>
-      <SideMenu></SideMenu>
-      </Grid>
-      <Grid container spacing={0.5} xs={12} md={9}>
-          <List></List>
-      </Grid>
-    </Grid>
+      <CssVarsProvider defaultMode="system">
 
-    </CssVarsProvider>
+        <Header></Header>
+
+        <div className='container'>
+          
+          <Slider></Slider>
+        <div className="overlay"></div>
+      
+        </div>
+        <Grid className="bg-dark py-4" container spacing={3} sx={{ flexGrow: 1 }}>
+
+          <Grid className="my-3" lg={12}>
+            <Typography
+              color="neutral"
+              level="h1"
+              textAlign={'center'}>
+              Most Valuable Properties of Kota under one Roof
+            </Typography>
+            <Typography className="mb-3" textAlign={'center'}>
+              At Afordable Price at <Chip> 0% brokerage </Chip>
+            </Typography>
+          </Grid>
+
+          <Grid xs={12} md={3}>
+            <SideMenu></SideMenu>
+          </Grid>
+
+          <Grid container spacing={0.5} xs={12} md={9}>
+            <List></List>
+          </Grid>
+
+        </Grid>
+
+      </CssVarsProvider>
     </>
   )
 }
--- frontent/src/comp/Header/header.jsx
+++ frontent/src/comp/Header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./header.css"
+import logo from "../../assets/logo.png"
 import { CiLocationOn } from "react-icons/ci";
 import { CiBookmarkPlus } from "react-icons/ci";
 import { CiCircleQuestion } from "react-icons/ci";
@@ -7,8 +8,10 @@ import menu from "../../assets/menu.svg"
 import { CiImageOn } from "react-icons/ci";
 import m2 from "../../assets/m2.svg"
 import m3 from "../../assets/m3.svg"
+import { FaWhatsapp } from "react-icons/fa";
 import m4 from "../../assets/m4.svg"
 import m5 from "../../assets/m5.svg"
+import { CiInstagram } from "react-icons/ci";
 import { PiBuildingsDuotone } from "react-icons/pi";
 
 const Header = (props) => {
@@ -19,25 +22,26 @@ const Header = (props) => {
 
   return (
     <div className='header p-3'>
-        <div className="row text-center border-bottom border-dark pb-2">
-         
-            <div className="col-10">
-           
-                <h1>CityDealer</h1>
+        <div className="row text-center border-bottom border-dark px-3">
+            <div className="col-1">
+                <img src={logo} className='w-100' alt="logo" />
             </div>
-            <div className="col-2 d-md-none d-lg-none  menu">
+            <div className="col-9 d-flex align-items-center">
+                <h1 >CityDealer</h1>
+            </div>
+            <div className="col-2 d-md-none d-lg-none menu">
                 <button type="button"><img src={menu} alt="menu" /></button>
             </div>
         </div>
 
-        <div className="row my-4 d-none d-md-flex justify-content-between  topMenu ">
+        <div className="row my-4 d-none d-md-flex justify-content-between topMenu ">
             <div className="col-6 d-flex justify-content-around">
                 <button ><CiImageOn /><p>Gallery</p></button>
                 <button type='button' ><CiBookmarkPlus /> <p> Saved</p></button>
-                <button ><CiLocationOn /><p>Map</p></button> 
+                <button ><FaWhatsapp /><p>whatsapp</p></button> 
             </div>
             <div className="col-4 d-flex justify-content-around pe-2">
-                <button ><img src={m5} alt="Sort" /></button>
+                <button ><CiInstagram/> <p>Instagram</p></button>
                 <button type='button' ><CiCircleQuestion /> <p>about us</p></button>
             </div>  
 
